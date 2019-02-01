ac5401be902cfb7b785e78fe1feba6492afc9d7f
Aaron <15201622482@163.com>
2019-02-01 14:58:53 +0800

    Add files via upload

---
+++ feature_training.cpp
@@ -0,0 +1,50 @@
+#include "DBoW3/DBoW3.h"
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <iostream>
+#include <vector>
+#include <string>
+#include "tic_toc.h"
+using namespace cv;
+using namespace std;
+
+/***************************************************
+ * 本节演示了如何根据data/目录下的十张图训练字典
+ * ************************************************/
+
+int main( int argc, char** argv )
+{
+    TicToc tr;
+    // read the image 
+    cout<<"reading images... "<<endl;
+    vector<Mat> images; 
+    for ( int i=0; i<221; i++ )
+    {
+        string path = "/home/aaron/slambook/slambook-master/picmatch/xiaomi2/"+to_string(i)+".jpg";	
+        images.push_back( imread(path) );
+    }
+    // detect ORB features
+    cout<<"read time spend "<<tr.toc()<<"ms"<<endl;
+    cout<<"detecting ORB features ... "<<endl;
+    Ptr < Feature2D > detector = ORB::create();
+    vector<Mat> descriptors;
+    for ( Mat& image:images )
+    {
+        vector<KeyPoint> keypoints; 
+        Mat descriptor;
+        detector->detectAndCompute( image, Mat(), keypoints, descriptor );
+        descriptors.push_back( descriptor );
+    }
+    cout<<"detect orb features spend "<<tr.toc()<<"ms"<<endl;
+    // create vocabulary 
+    cout<<"creating vocabulary ... "<<endl;
+    DBoW3::Vocabulary vocab;
+    vocab.create( descriptors );
+    cout<<"vocabulary info: "<<vocab<<endl;
+    vocab.save( "./vocabulary22.yml.gz" );
+    cout<<"detect orb features spend "<<tr.toc()<<"ms"<<endl;
+    cout<<"done"<<endl;
+    
+    return 0;
+}
